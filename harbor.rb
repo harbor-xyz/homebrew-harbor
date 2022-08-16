ae0735fd1ce6b57590f3f47eabb80a469c23a5e4
prakashsanker <praks5432@gmail.com>
2022-08-16 21:52:52 +0530

    Brew formula update for harbor-cli version v0.2.2

--- harbor.rb
+++ harbor.rb
@@ -5,20 +5,20 @@
 class Harbor < Formula
   desc ""
   homepage "https://github.com/harbor-xyz/harbor-cli"
-  version "0.2.1"
+  version "0.2.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/harbor-xyz/harbor-cli/releases/download/v0.2.1/harbor-cli_0.2.1_darwin_arm64.tar.gz"
-      sha256 "f6830e7e06ba853fe4fe14d7b1a6661fa62f8e7aa6395501eb66ae89173d6c7c"
+      url "https://github.com/harbor-xyz/harbor-cli/releases/download/v0.2.2/harbor-cli_0.2.2_darwin_arm64.tar.gz"
+      sha256 "c820412a67af806317322ff61a3ac7de1b22df7a8924276bde1d6e6d78152583"
 
       def install
         bin.install "harbor"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/harbor-xyz/harbor-cli/releases/download/v0.2.1/harbor-cli_0.2.1_darwin_amd64.tar.gz"
-      sha256 "7451fdc39291eb44f71685eacd602768959ddd311ebe87ca9ea6fd6ef6a9e5f0"
+      url "https://github.com/harbor-xyz/harbor-cli/releases/download/v0.2.2/harbor-cli_0.2.2_darwin_amd64.tar.gz"
+      sha256 "be71012bb6925de51956a154dbafcdeaad1b5efffb383c495d4f71fd16d09f4f"
 
       def install
         bin.install "harbor"
@@ -28,16 +28,16 @@ class Harbor < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/harbor-xyz/harbor-cli/releases/download/v0.2.1/harbor-cli_0.2.1_linux_arm64.tar.gz"
-      sha256 "13833581a9fb504c726b398a0a9432d87a56fd365a2bbe1d4a0eaa58ccca642f"
+      url "https://github.com/harbor-xyz/harbor-cli/releases/download/v0.2.2/harbor-cli_0.2.2_linux_arm64.tar.gz"
+      sha256 "90c0f1c743477ab64290b6299b5565754fdf989379d46799f1e72fbae084fb87"
 
       def install
         bin.install "harbor"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/harbor-xyz/harbor-cli/releases/download/v0.2.1/harbor-cli_0.2.1_linux_amd64.tar.gz"
-      sha256 "90ea49d0cc79f21a7d3f6e47c7b0247d5d8fae1d00d421e541b402ebbe23c265"
+      url "https://github.com/harbor-xyz/harbor-cli/releases/download/v0.2.2/harbor-cli_0.2.2_linux_amd64.tar.gz"
+      sha256 "544646f7e404b71d3a078330dd484d4c89588501f21004a576171f7eab917101"
 
       def install
         bin.install "harbor"
