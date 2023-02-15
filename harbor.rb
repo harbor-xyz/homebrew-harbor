9cd29f8e25f9071e80702a20795650cb0e443555
prakashsanker <praks5432@gmail.com>
2023-02-15 19:25:37 +0530

    Brew formula update for harbor-cli version v0.4.6

--- harbor.rb
+++ harbor.rb
@@ -5,20 +5,20 @@
 class Harbor < Formula
   desc ""
   homepage "https://github.com/harbor-xyz/harbor-cli"
-  version "0.4.5"
+  version "0.4.6"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/harbor-xyz/harbor-cli/releases/download/v0.4.5/harbor-cli_0.4.5_darwin_arm64.tar.gz"
-      sha256 "da1ae5d968cd1f66b1800532c96afd5117a2a1daa339c26c67a170551340e2e8"
+      url "https://github.com/harbor-xyz/harbor-cli/releases/download/v0.4.6/harbor-cli_0.4.6_darwin_arm64.tar.gz"
+      sha256 "9f2080747084dc2174bf0cfb990fe9da46bb16ac27939f5fef25829099c906c9"
 
       def install
         bin.install "harbor"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/harbor-xyz/harbor-cli/releases/download/v0.4.5/harbor-cli_0.4.5_darwin_amd64.tar.gz"
-      sha256 "fdce29dd5000a25785a061e6137625d0c96318c5d8edf5d63199693f16455ae0"
+      url "https://github.com/harbor-xyz/harbor-cli/releases/download/v0.4.6/harbor-cli_0.4.6_darwin_amd64.tar.gz"
+      sha256 "1c3b2822c718c9074b557ecb2175d441af132de5ab06da27529f7686c003bab3"
 
       def install
         bin.install "harbor"
@@ -28,20 +28,31 @@ class Harbor < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/harbor-xyz/harbor-cli/releases/download/v0.4.5/harbor-cli_0.4.5_linux_arm64.tar.gz"
-      sha256 "36e998991c995dab1a8443eecbf66a053c872c70c3d9c2f7222b3e3d7ff2e788"
+      url "https://github.com/harbor-xyz/harbor-cli/releases/download/v0.4.6/harbor-cli_0.4.6_linux_arm64.tar.gz"
+      sha256 "79cd84c28c318366e2c9849fb2ac72300855d475f59f5da12bb81ba6c128f0e3"
 
       def install
         bin.install "harbor"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/harbor-xyz/harbor-cli/releases/download/v0.4.5/harbor-cli_0.4.5_linux_amd64.tar.gz"
-      sha256 "1b23ccb0976184a402b1a759b10d973f230f7f235b6e84cd516a06db3d369b20"
+      url "https://github.com/harbor-xyz/harbor-cli/releases/download/v0.4.6/harbor-cli_0.4.6_linux_amd64.tar.gz"
+      sha256 "be3478bdf565f37d4af6efc906813adc9526ee1286eab75df21c09eef8a5b9b8"
 
       def install
         bin.install "harbor"
       end
     end
+  end
+
+  def caveats
+    <<~EOS
+      🙂 Congratulation on installation of the harbor cli.
+      🗲 Start with the command `harbor apply <testnet-name> --config <path/to/config/file>` to quickly get started.
+      🛈 To see advanced option and detailed documentation, visit https://docs.goharbor.com/docs/installation
+
+      🗧 You can optionally run command `harbor completion bash > /etc/bash_completion.d/harbor_completion` to enable auto-complete for future. You will need to select appropriate prompt and should have required permissions.
+      🖒 Feel free to drop a note to share any issues / concerns / suggestion at hello@goharbor.xyz
+    EOS
   end
 end
